8d4636d13d16ac28efa2ff864d369a8da72eed48
Music For Me <nnam18740@gmail.com>
2024-06-07 13:16:45 +0700

    Initial commit with project files

---
+++ config.js
@@ -0,0 +1,9 @@
+const nameGirl = 'Bùi Thị Kiều Trang';
+const giftUrl = '#';
+const eventName = 'Happy Birthday❤️';
+const titleCard = 'Tặng Cục Cưng 1 món quà nè❤️';
+const contentCard = 'Chúc bé iu Bùi Thị Kiều Trang của anh sinh nhật tràn ngập niềm vui và những nụ cười. Mong điều đẹp nhất sẽ đến với bé trong hôm nay và cả những ngày sau nữa. Chúc bé đậu Nguyện Vọng 1';
+
+const giftImage = 'hot-girl.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
